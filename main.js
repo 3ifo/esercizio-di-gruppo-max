94f60e2829e389582ea15d3d4b4c7ab0cbb056aa
leonardo <leonardo_trifiletti@yahoo.it>
2023-10-24 16:11:21 +0200

    terminato es3

---
+++ main.js
@@ -0,0 +1,30 @@
+/* let numeroUtente = prompt("scegli un numero da 1 a 10");
+let numeroUtenteNumero = Number(numeroUtente);
+let i = 1;
+while (i <= 10) {
+  let moltiplicazione = numeroUtenteNumero * i;
+  console.log(numeroUtente + "x" + i + "=" + moltiplicazione);
+  i++;
+} */
+
+/* Sostitutore di parole 
+
+let prompt1 = prompt("Scrivi una frase");
+let prompt2 = prompt("Scegli una parola da cambiare");
+let prompt3 = prompt("Scegli un altra parola");
+
+let fraseCambiata = prompt1.replaceAll(prompt2, prompt3);
+
+console.log(fraseCambiata); */
+
+/* Verifica http https .com 
+
+let url = prompt("Scrivi un URL");
+if (
+  (url.startsWith("http://") || url.startsWith("https://")) &&
+  url.endsWith(".com")
+) {
+  alert("tutto ok");
+} else {
+  alert("errore");
+} */
